9a4fbe8db4d58c4a8dca82dbe398817b2df9ffb1
anirudh-bukka <anirudh.bukka@gmail.com>
2022-08-29 22:01:04 +0530

    LeetCode: Min cost problem

---
+++ Arrays/MinCostToMove.java
@@ -0,0 +1,16 @@
+// Link: https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+
+import java.util.*;
+
+public class MinCostToMove {
+    public static void main(String[] args) {
+        int[] ar = {1, 2, 3};
+        // This means, at 0th index, we have one item; at 1st index we have one item; at 2nd index, we have one item.
+
+        System.out.println(minCostToMove(ar));
+    }
+
+    public static int minCostToMove(int[] ar) {
+        return 0;
+    }
+}
---
+++ Sorting/QuickSort.java
@@ -0,0 +1,44 @@
+package Sorting;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] ar = {1, 2, 5, 2, 3};
+        int size = ar.length;
+        quickSort(ar, 0, size-1);
+        System.out.println("Sorted array: ");
+        
+        for(int i = 0; i < size; i++) 
+            System.out.print(ar[i] + " ");
+    }
+
+    static int partition(int[] ar, int low, int high) {
+        int pivot = ar[high];
+        int i = low-1;
+
+        for(int j = low; j <= high - 1; j++) {
+            if(ar[j] < pivot) {
+                i++;
+                swap(ar, i, j);
+            }
+        }
+        swap(ar, i+1, high);
+        return i+1;
+
+    }
+
+    static void quickSort(int[] ar, int low, int high) {
+        if(low < high) {
+            int p = partition(ar, low, high);
+            quickSort(ar, low, p-1);
+            quickSort(ar, p+1, high);
+        }
+    }
+    
+    static void swap(int[] ar, int i, int j) {
+        int temp = ar[i];
+        ar[i] = ar[j];
+        ar[j] = temp;
+    }
+
+}
