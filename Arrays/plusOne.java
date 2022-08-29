d64bad5db5f80d127f09c3803294825a4d803747
anirudh-bukka <anirudh.bukka@gmail.com>
2022-08-29 07:14:01 +0530

    Leetcode: Plus One

--- Arrays/plusOne.java
+++ Arrays/plusOne.java
@@ -2,7 +2,9 @@
 
 public class plusOne {
     public static void main(String[] args) {
-        
+        int[] ar = {1, 2, 3};
+        System.out.println(ar);
+        // output should be {1, 2, 4};
     }
     
 }
