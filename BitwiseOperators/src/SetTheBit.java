522af46ce0abaa341aad1b49d470890a27cda009
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-05 14:46:56 +0530

    Set and Reset bit

---
+++ BitwiseOperators/src/SetTheBit.java
@@ -0,0 +1,18 @@
+public class SetTheBit {
+    public static void main(String[] args) {
+        int number = 22;
+        int bit = 4;
+        System.out.println(setBit(number, bit));
+        System.out.println(resetBit(30, 4));
+    }
+
+    public static int setBit(int n, int bit) {
+        int mask = 1 << (bit - 1);
+        return n|mask;
+    }
+
+    public static int resetBit(int n, int bit) {
+        int mask = 1 << (bit - 1);
+        return n^mask;
+    }
+}
