5763c4bb600fc7db42b3d4b0d8fa5120fdab62e9
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-28 00:21:04 +0530

    Multiple commits

--- Recursion/src/CheckSort.java
+++ Recursion/src/CheckSort.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class CheckSort {
+public class CheckSort {
+    public static void main(String[] args) {
+        int[] ar = {1, 2, 4, 5, 6, 10};
+        System.out.println(checkSort(ar, 0));
+    }
+
+    public static boolean checkSort(int[] ar, int i) {
+
+        if(i == ar.length - 1)
+            return true;
+
+        return ar[i] < ar[i+1] && checkSort(ar, i+1);
+    }
 }
--- Recursion/src/SpacesCleaningRobot.java
+++ Recursion/src/SpacesCleaningRobot.java
@@ -1,4 +1,7 @@
 // Link: https://leetcode.com/problems/number-of-spaces-cleaning-robot-cleaned/
 
 public class SpacesCleaningRobot {
+    public static void main(String[] args) {
+
+    }
 }
