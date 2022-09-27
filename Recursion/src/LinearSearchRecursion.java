956fb692a1ea201996879f0f97c574e08bc8b09b
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-27 22:42:09 +0530

    IMP: First-Last occurrence - with list (Recursion)

--- Recursion/src/LinearSearchRecursion.java
+++ Recursion/src/LinearSearchRecursion.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class LinearSearchRecursion {
     public static void main(String[] args) {
-        int[] ar = {1, 5, 2, 6, 6, 3, 4};
+        int[] ar = {6, 1, 5, 2, 6, 6, 6, 3, 4};
         int target = 6;
+
         System.out.println("First occurrence: " + linearSearch(ar, target, 0));
         System.out.println("Last occurence: " + linearSearchFromEnd(ar, target, ar.length-1));
+        System.out.println("List of elements present: " + returnList(ar, target, 0, new ArrayList<>()));
     }
 
     static int linearSearch(int[] ar, int target, int i) {
@@ -25,5 +29,17 @@ public class LinearSearchRecursion {
             return i;
 
         return linearSearchFromEnd(ar, target, i-1);
+    }
+
+
+    static ArrayList returnList(int[] ar, int target, int i, ArrayList<Integer> list) {
+        if(i == ar.length)
+            return list;
+
+        if(target == ar[i]) {
+            list.add(i);
+        }
+
+        return returnList(ar, target, i+1, list);
     }
 }
