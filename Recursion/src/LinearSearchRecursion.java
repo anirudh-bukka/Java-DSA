76ee160e444f7394e8ddcb0cfa3b1a6840213580
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-27 21:12:08 +0530

    Linear Search using recursion.

--- Recursion/src/LinearSearchRecursion.java
+++ Recursion/src/LinearSearchRecursion.java
@@ -6,9 +6,6 @@ public class LinearSearchRecursion {
     }
 
     static int linearSearch(int[] ar, int target, int i) {
-//        if(ar[i] == target)
-//            return i;
-//        return linearSearch(ar, target, i+1);
 
         if(i == ar.length)
             return -1;
