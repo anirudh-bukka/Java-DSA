d688395bc60b57a82be99d88eb49b873a6a2e977
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-27 21:25:09 +0530

    First and Last Occurance using Linear Search.

--- Recursion/src/LinearSearchRecursion.java
+++ Recursion/src/LinearSearchRecursion.java
@@ -1,8 +1,9 @@
 public class LinearSearchRecursion {
     public static void main(String[] args) {
-        int[] ar = {1, 5, 2, 6, 3, 4};
-        int target = 2;
-        System.out.println("Present at index: " + linearSearch(ar, target, 0));
+        int[] ar = {1, 5, 2, 6, 6, 3, 4};
+        int target = 6;
+        System.out.println("First occurrence: " + linearSearch(ar, target, 0));
+        System.out.println("Last occurence: " + linearSearchFromEnd(ar, target, ar.length-1));
     }
 
     static int linearSearch(int[] ar, int target, int i) {
@@ -14,5 +15,15 @@ public class LinearSearchRecursion {
             return i;
 
         return linearSearch(ar, target, i+1);
+    }
+
+    static int linearSearchFromEnd(int[] ar, int target, int i) {
+        if(i == -1)
+            return -1;
+
+        if(ar[i] == target)
+            return i;
+
+        return linearSearchFromEnd(ar, target, i-1);
     }
 }
