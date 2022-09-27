1df00781a742e4729c5af7f7a3291acda05d5ac6
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-28 00:19:55 +0530

    ***IMP: First-Last index;List of indices-recursion

--- Recursion/src/LinearSearchRecursion.java
+++ Recursion/src/LinearSearchRecursion.java
@@ -8,6 +8,7 @@ public class LinearSearchRecursion {
         System.out.println("First occurrence: " + linearSearch(ar, target, 0));
         System.out.println("Last occurence: " + linearSearchFromEnd(ar, target, ar.length-1));
         System.out.println("List of elements present: " + returnList(ar, target, 0, new ArrayList<>()));
+        System.out.println("List of elements present: " + returnList2(ar, target, 0));
     }
 
     static int linearSearch(int[] ar, int target, int i) {
@@ -31,7 +32,10 @@ public class LinearSearchRecursion {
         return linearSearchFromEnd(ar, target, i-1);
     }
 
-
+    // By passing the ArrayList as a parameter
+        /* In this method/function, although the list is getting called every recursion call,
+         it is referring to the same/previously updated list. This means, the list is carrying all the previous values
+         for every new call, which is a waste of space and inefficient performance. */
     static ArrayList returnList(int[] ar, int target, int i, ArrayList<Integer> list) {
         if(i == ar.length)
             return list;
@@ -41,5 +45,24 @@ public class LinearSearchRecursion {
         }
 
         return returnList(ar, target, i+1, list);
+    }
+
+    // Without passing ArrayList as a parameter
+        /* In this method/function, new list is created with every recursion call. This means the value is appended in
+         the list if satisfied, and in the end, adds all the individual lists into one list. */
+    static ArrayList<Integer> returnList2(int[] ar, int target, int i) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        if(i == ar.length)
+            return list;
+
+        if(ar[i] == target)
+            list.add(i);
+
+        ArrayList<Integer> ansFromBelowCalls = returnList2(ar, target, i+1);
+
+        list.addAll(ansFromBelowCalls);
+
+        return list;
     }
 }
