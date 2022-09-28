cf60d2cf40a7683d88219ccb2de2761723fe1437
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-28 23:17:10 +0530

    Bottom Left Triangle - Recursion

--- Recursion/src/BottomLeftTriangle.java
+++ Recursion/src/BottomLeftTriangle.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class BottomLeftTriangle {
+public class BottomLeftTriangle {
+    public static void main(String[] args) {
+        int rows = 3;
+        int columns = 0;
+        generateTriangle(rows, columns);
+    }
+
+    static void generateTriangle(int r, int c) {
+        if(r == 0)
+            return;
+        if(c < r) {
+            generateTriangle(r, c+1);
+            System.out.print("*");
+        }
+        else {
+            generateTriangle(r-1, 0);
+            System.out.println();
+        }
+    }
 }
+/*
+
+*
+**
+***
+
+*/
