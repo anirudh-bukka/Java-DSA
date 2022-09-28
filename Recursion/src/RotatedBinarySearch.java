a3df69261d615c2f308f49123555f4a45bb12c28
anirudh-bukka <anirudh.bukka@gmail.com>
2022-09-28 07:15:53 +0530

    Rotated Binary Search - Recursion.

--- Recursion/src/RotatedBinarySearch.java
+++ Recursion/src/RotatedBinarySearch.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class RotatedBinarySearch {
+public class RotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] ar = {5, 6, 7, 8, 9, 1, 2, 3, 4};
+        int target = 8;
+        System.out.println(search(ar, target, 0, ar.length-1));
+    }
+
+    static int search(int[] ar, int target, int start, int end) {
+        if (start > end)
+            return -1;
+
+        int mid = start + (end - start) / 2;
+        if (ar[mid] == target)
+            return mid;
+
+        if (ar[start] <= ar[mid]) {
+            if (target >= ar[start] && target <= ar[mid]) // this means, this part of the array is sorted.
+                search(ar, target, start, mid - 1);
+
+            else
+                return search(ar, target, mid + 1, end);
+        }
+
+        if(target >= ar[mid] && target <= ar[end])
+            return search(ar, target, mid + 1, end);
+
+        return search(ar, target, start, mid - 1);
+    }
+
 }
