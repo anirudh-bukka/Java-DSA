5a5a478b0e832b36a5fba7ea1b5c33a7e1dc31ff
anirudh-bukka <anirudh.bukka@gmail.com>
2022-10-04 19:50:29 +0530

    No. of subsets that add to a target on a die.

--- Subset-Subsequence/src/Dice.java
+++ Subset-Subsequence/src/Dice.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Dice {
+import java.util.ArrayList;
+
+public class Dice {
+    public static void main(String[] args) {
+        diceVoid("", 4);
+        System.out.println(diceList("", 4));
+        int[] ar = {2, 3, 4, 5};
+        System.out.println(diceListOfLists("", ar, 5));
+    }
+
+    static void diceVoid(String p, int target) {
+        if(target == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for(int i = 1; i <= 6 && i <= target; i++)
+            diceVoid(p+i, target - i);
+
+
+    }
+
+    static ArrayList<String> diceList(String p, int target) {
+        if(target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 1; i <= 6 && i <= target; i++) {
+            list.addAll(diceList(p+i, target-i));
+        }
+
+        return list;
+    }
+
+    static ArrayList<String> diceListOfLists(String p, int[] ar, int target) {
+        if(target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i < ar.length; i++) {
+            list.addAll(diceListOfLists(p+ar[i], ar,target-ar[i]));
+        }
+        return list;
+    }
 }
