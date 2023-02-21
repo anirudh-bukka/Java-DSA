9a68e1ee21eba4c2ad0bfdc0ca7dbd06d5e708dd
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-21 21:50:48 +0530

    Queue classes implementation.

--- Stack-Queue/src/QueueMain.java
+++ Stack-Queue/src/QueueMain.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class QueueMain {
+public class QueueMain {
+    public static void main(String[] args) throws QueueException {
+        CustomQueue queue = new CustomQueue(5);
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+        queue.insert(4);
+        queue.insert(5);
+
+        System.out.print("Entire Queue: ");
+        queue.display();
+        System.out.println("Front of queue: " + queue.displayFront());
+
+        queue.remove();
+        queue.remove();
+        queue.remove();
+
+        System.out.print("Entire Queue: ");
+        queue.display();
+        System.out.println("Front of queue: " + queue.displayFront());
+    }
 }
