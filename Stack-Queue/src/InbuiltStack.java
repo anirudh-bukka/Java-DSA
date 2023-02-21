43ba11b2007bd17b3009d144b745b478208d398d
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-21 20:47:45 +0530

    Built-in stack class.

--- Stack-Queue/src/InbuiltStack.java
+++ Stack-Queue/src/InbuiltStack.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class InbuiltStack {
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class InbuiltStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack  = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
 }
