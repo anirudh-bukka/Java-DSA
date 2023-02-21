9e5618904d07876e9fc881e7389b6e907a1355a3
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-21 20:46:38 +0530

    Stack with dynamic size.

--- Stack-Queue/src/DynamicStack.java
+++ Stack-Queue/src/DynamicStack.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class DynamicStack {
+public class DynamicStack extends CustomStack {
+    public DynamicStack() {
+        super(); // It will call CustomStack();
+    }
+
+    public DynamicStack(int size) {
+        super(size); // It will call CustomStack(int size);
+    }
+
+    // In dynamic stack, every method of CustomStack class will be same except for `push(int item)`.
+
+    @Override
+    public boolean push(int item) {
+        if(this.isFull()) {
+            int[] newArray = new int[data.length * 2];
+
+            // copy all previous items into newArray
+            for(int i = 0; i < data.length; i++) {
+                newArray[i] = data[i];
+            }
+
+            data = newArray;
+        }
+
+        // At this point we know that the array is not full.
+        // Insert
+        return super.push(item);
+    }
 }
