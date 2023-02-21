925cfbd1a38af49337341ef7ad4355dc289644c6
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-22 00:02:30 +0530

    Implementation of Circular Queue.

--- Stack-Queue/src/CircularQueueMain.java
+++ Stack-Queue/src/CircularQueueMain.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CircularQueueMain {
+public class CircularQueueMain {
+    public static void main(String[] args) throws CircularQueueException {
+        CustomCircularQueue queue = new CustomCircularQueue(5);
+
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+        queue.insert(4);
+        queue.insert(5);
+
+        queue.display();
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+
+        queue.display();
+
+        queue.insert(99);
+        queue.display();
+        queue.insert(1919);
+        queue.display();
+    }
 }
