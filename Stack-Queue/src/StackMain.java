e1684e6286b848a4cb33e2fd7a6899d86037b0ac
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-21 20:50:07 +0530

    Main class (implementation of all stack classes defined).

--- Stack-Queue/src/StackMain.java
+++ Stack-Queue/src/StackMain.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class StackMain {
+public class StackMain {
+    public static void main(String[] args) throws StackException {
+
+        // CustomStack stack = new CustomStack(5);
+        DynamicStack stack = new DynamicStack(5);
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(5);
+        stack.push(5);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+    }
 }
