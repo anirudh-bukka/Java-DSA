5cf77184bd7ee737e44de0dde247c10025d47701
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-22 21:42:14 +0530

    Circular & Dynamic Queue implementation.

--- Stack-Queue/src/CircularQueueMain.java
+++ Stack-Queue/src/CircularQueueMain.java
@@ -1,7 +1,7 @@
 public class CircularQueueMain {
     public static void main(String[] args) throws CircularQueueException {
-        CustomCircularQueue queue = new CustomCircularQueue(5);
-
+//        CustomCircularQueue queue = new CustomCircularQueue(5);
+        DynamicQueue queue = new DynamicQueue(3);
         queue.insert(1);
         queue.insert(2);
         queue.insert(3);
