23bd869f8c65a8f55364f3df0cdae2cf35741614
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-28 08:30:59 +0530

    VVV Good Q on STACK.

--- LeetCode-Questions/src/stacks_queues/Asteroids.java
+++ LeetCode-Questions/src/stacks_queues/Asteroids.java
@@ -1,2 +1,44 @@
-package stacks_queues;public class Asteroids {
+package stacks_queues;
+
+import java.util.Stack;
+
+public class Asteroids {
+    public static void main(String[] args) {
+        int[] ar = {5, 10, -5};
+        System.out.println(asteroidCollision(ar));
+    }
+
+    public static int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < asteroids.length; i++) {
+            if(stack.isEmpty() || asteroids[i]>0) {
+                stack.push(asteroids[i]);
+            } else {
+                while(true) {
+                    int peek = stack.peek();
+                    if(peek < 0) {
+                        stack.push(asteroids[i]);
+                        break;
+                    } else if(peek == -asteroids[i]) {
+                        stack.pop();
+                        break;
+                    } else if(peek > -asteroids[i]){
+                        break;
+                    } else {
+                        stack.pop();
+                        if(stack.isEmpty()) {
+                            stack.push(asteroids[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+        for(int i = stack.size()-1; i >= 0; i--) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
 }
