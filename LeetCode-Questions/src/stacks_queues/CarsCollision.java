3ee57004c6a624245c6380c94c4dfb796b348f27
anirudh-bukka <anirudh.bukka@gmail.com>
2023-02-28 17:59:17 +0530

    [INCOMPLETE] VVV Good Q on STACK.

--- LeetCode-Questions/src/stacks_queues/CarsCollision.java
+++ LeetCode-Questions/src/stacks_queues/CarsCollision.java
@@ -10,30 +10,44 @@ public class CarsCollision {
     public static int countCollisions(String directions) {
         Stack<Character> stack = new Stack<>();
 
-        for(char c : directions.toCharArray()) {
-            stack.push(c);
+        char[] directionsArray = directions.toCharArray();
+        for(int i = directionsArray.length - 1; i >= 0; i--) {
+            stack.push(directionsArray[i]);
         }
+//        for(char c : directions.toCharArray()) {
+//            stack.push(c);
+//        }
 
         int count = 0;
 
-        if(stack.pop() == 'R') {
-            if(stack.peek() == 'L') {
-                count += 2;
-                stack.set(-1, 'S');
-            } else if(stack.peek() == 'S') {
-                count += 1;
-            } else {
-                count += 0;
+        while(!stack.isEmpty()) {
+            if (stack.pop() == 'R') {
+                if (stack.peek() == 'L') {
+                    count += 2;
+                    stack.set(stack.size()-1, 'S');
+//                    stack.set(stack.indexOf(stack.peek()), 'S');
+                } else if (stack.peek() == 'S') {
+                    count += 1;
+                } else {
+                    count += 0;
+                }
             }
-        } else if(stack.pop() == 'L') {
-            count += 0;
-        } else if(stack.pop() == 'S') {
-            if(stack.peek() == 'L') {
-                count += 1;
-                stack.set(-1, 'S');
-            } else if() {
-
+            if (stack.peek() == 'L') {
+//                stack.pop();
+                count += 0;
+//                continue;
+            }
+            if (stack.peek() == 'S') {
+                stack.pop();
+                if (stack.peek() == 'L') {
+                    count += 1;
+                    stack.set(stack.size()-1, 'S');
+//                    stack.set(stack.indexOf(stack.peek()), 'S');
+                } else if (stack.peek() == 'R') {
+                    count += 0;
+                }
             }
         }
+        return count;
     }
 }
