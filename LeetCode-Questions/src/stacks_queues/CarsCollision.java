0887cd2059ce1153876fde2f930b4410daab43d7
anirudh-bukka <anirudh.bukka@gmail.com>
2023-03-01 07:31:36 +0530

    [VVV Good Question - W/O stack.

--- LeetCode-Questions/src/stacks_queues/CarsCollision.java
+++ LeetCode-Questions/src/stacks_queues/CarsCollision.java
@@ -8,52 +8,78 @@ public class CarsCollision {
         System.out.println(countCollisions(s));
     }
     public static int countCollisions(String directions) {
-        Stack<Character> stack = new Stack<>();
 
-        char[] directionsArray = directions.toCharArray();
-        for(int i = directionsArray.length - 1; i >= 0; i--) {
-            stack.push(directionsArray[i]);
-        }
-//        for(char c : directions.toCharArray()) {
-//            stack.push(c);
-//        }
+//        Correct Answer:
+            int left = 0, right = directions.length() - 1;
 
-        int count = 0;
-        int ptr = stack.size();
-
-        while (!stack.isEmpty()) {
-//        while(ptr >= 0) {
-            if(stack.peek() == 'R') {
-                if(stack.indexOf(stack.peek() - 1) == 'L') {
-                    count += 2;
-                    stack.pop();
-                    stack.set(stack.peek(), 'S');
-                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'S') {
-                    count += 1;
-                    stack.pop();
-                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'R') {
-                    count += 0;
-                }
-                ptr--;
-            } else if(stack.peek() == 'L') {
-                if(stack.get(stack.indexOf(stack.peek()) - 1) == 'R') {
-                    count += 0; // Because 'L' goes left and 'R' goes Right and they never meet;
-                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'S') {
-                    count += 0;
-                }
-                stack.pop();
-                ptr--;
-            } else if(stack.peek() == 'S') {
-                if(stack.get(stack.indexOf(stack.peek()) - 1) == 'L') {
-                    count += 1;
-                    stack.set(stack.peek() - 1, 'S');
-                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'R') {
-                    count += 0; // Because 'S' remains but 'R' goes Right and they never meet;
-                }
-                stack.pop();
-                ptr--;
+            while (left < directions.length() && directions.charAt(left) == 'L') {
+                left++;
             }
+
+            while (right >= 0 && directions.charAt(right) == 'R') {
+                right--;
+            }
+
+            int count = 0;
+            for (int i = left; i <= right; i++) {
+                if (directions.charAt(i) != 'S') {
+                    count++;
+                }
+            }
+            //combining these three loops - TC : O(N).
+
+            return count;
         }
-        return count;
+
+        // *****************************************************************************
+
+
+//        Stack<Character> stack = new Stack<>();
+//
+//        char[] directionsArray = directions.toCharArray();
+//        for(int i = directionsArray.length - 1; i >= 0; i--) {
+//            stack.push(directionsArray[i]);
+//        }
+////        for(char c : directions.toCharArray()) {
+////            stack.push(c);
+////        }
+//
+//        int count = 0;
+//        int ptr = stack.size();
+//
+//        while (!stack.isEmpty()) {
+////        while(ptr >= 0) {
+//            if(stack.peek() == 'R') {
+//                if(stack.indexOf(stack.peek() - 1) == 'L') {
+//                    count += 2;
+//                    stack.pop();
+//                    stack.set(stack.peek(), 'S');
+//                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'S') {
+//                    count += 1;
+//                    stack.pop();
+//                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'R') {
+//                    count += 0;
+//                }
+//                ptr--;
+//            } else if(stack.peek() == 'L') {
+//                if(stack.get(stack.indexOf(stack.peek()) - 1) == 'R') {
+//                    count += 0; // Because 'L' goes left and 'R' goes Right and they never meet;
+//                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'S') {
+//                    count += 0;
+//                }
+//                stack.pop();
+//                ptr--;
+//            } else if(stack.peek() == 'S') {
+//                if(stack.get(stack.indexOf(stack.peek()) - 1) == 'L') {
+//                    count += 1;
+//                    stack.set(stack.peek() - 1, 'S');
+//                } else if(stack.get(stack.indexOf(stack.peek()) - 1) == 'R') {
+//                    count += 0; // Because 'S' remains but 'R' goes Right and they never meet;
+//                }
+//                stack.pop();
+//                ptr--;
+//            }
+//        }
+//        return count;
     }
 }
