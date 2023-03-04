f3d527dad2c112ce28a6b8f7220bd335483a78e7
anirudh-bukka <anirudh.bukka@gmail.com>
2023-03-04 07:38:12 +0530

    shit easy - check string palindrome.

--- DSA-Sheet/src/strings/checkPalindrome.java
+++ DSA-Sheet/src/strings/checkPalindrome.java
@@ -1,2 +1,30 @@
-package strings;public class checkPalindrome {
+package strings;
+
+public class checkPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("abbac"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int n = s.length();
+        for(int i = 0; i < n/2; i++) {
+            if(s.charAt(i) == s.charAt(n - i - 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
+/*
+Example 1:
+Input: S = "abba"
+Output: 1
+Explanation: S is a palindrome
+
+Example 2:
+Input: S = "abc"
+Output: 0
+Explanation: S is not a palindrome
+ */
