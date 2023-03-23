2846d60d391eb8fcd21f0636698249a4b5693821
anirudh-bukka <anirudh.bukka@gmail.com>
2023-03-23 21:15:39 +0530

    Print path of travel based on tickets (which have start and destination).

--- HashSet_Map_Table/src/ItineraryFromTickets.java
+++ HashSet_Map_Table/src/ItineraryFromTickets.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class ItineraryFromTickets {
+import java.util.HashMap;
+
+public class ItineraryFromTickets {
+
+    public static String getStart(HashMap<String, String> tickets) {
+        HashMap<String, String> reverseMap = new HashMap<>();
+
+        for(String key : tickets.keySet()) {
+            reverseMap.put(tickets.get(key), key);
+        }
+
+        for(String key : tickets.keySet()) {
+            if(!reverseMap.containsKey(key))
+                return key;
+        }
+        return null;
+    }
+
+    public static void itinerary(String start, HashMap<String, String> tickets) {
+        while(tickets.containsKey(start)) {
+            System.out.print(start + " -> ");
+            start = tickets.get(start);
+        }
+        System.out.print(start + " -> ");
+        System.out.println("End.");
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put ("Chennai", "Bengaluru");
+        tickets.put ("Mumbai", "Delhi");
+        tickets.put ("Goa", "Chennai");
+        tickets.put ("Delhi", "Goa");
+
+        String startCity = getStart(tickets);
+        System.out.println("Start: " + startCity);
+        itinerary(startCity, tickets);
+    }
 }
