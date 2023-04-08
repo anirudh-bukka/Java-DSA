fce25352a950044686fa5d166de6568e87e04115
anirudh-bukka <anirudh.bukka@gmail.com>
2023-04-08 19:23:12 +0530

    [V.V.V Imp -- JPMC]

---
+++ DSA-Sheet/src/Practice/BreakPalindrome.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class BreakPalindrome {
+    public static void main(String[] args) {
+        String s = "aaabbaaa";
+        System.out.println(breakPalindrome(s));
+    }
+
+    public static String breakPalindrome(String s) {
+        char[] ar = s.toCharArray();
+        int n = ar.length;
+        for(int i = 0; i < n/2; i++) {
+            if(ar[i] != 'a') {
+                ar[i] = 'a';
+                return String.valueOf(ar);
+            }
+        }
+//        ar[n-1] = 'b';
+        return n <= 1 ? "" : "IMPOSSIBLE";
+    }
+}
