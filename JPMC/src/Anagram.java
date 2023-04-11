837051de1c1810f5a16c317f537162826fceaf3d
anirudh-bukka <anirudh.bukka@gmail.com>
2023-04-11 18:05:12 +0530

    HashMap

--- JPMC/src/Anagram.java
+++ JPMC/src/Anagram.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 // Link: https://www.geeksforgeeks.org/check-whether-two-strings-are-anagram-of-each-other/
 
@@ -8,21 +9,28 @@ public class Anagram {
         System.out.println(checkAnagram(s1, s2));
     }
 
-    public static boolean checkAnagram(String s1, String s2) {
-        if(s1.length() != s2.length())
+    public static boolean checkAnagram(String s, String t) {
+        if(s.length() != t.length())
             return false;
 
-        HashSet<Character> set1 = new HashSet<>();
-        HashSet<Character> set2 = new HashSet<>();
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
 
-        for(char c : s1.toCharArray())
-            set1.add(c);
-        for(char c : s2.toCharArray())
-            set2.add(c);
+        for(char c : s.toCharArray()) {
+            if(!map1.containsKey(c))
+                map1.put(c, 1);
+            else
+                map1.put(c, map1.get(c) + 1);
+        }
+        for(char c : t.toCharArray()) {
+            if(!map2.containsKey(c))
+                map2.put(c, 1);
+            else
+                map2.put(c, map2.get(c) + 1);
+        }
 
-        if(set1.equals(set2))
+        if(map1.equals(map2))
             return true;
-
         return false;
     }
 }
