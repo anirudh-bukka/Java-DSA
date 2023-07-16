8bbb419557620a81daf84030d0fce1233f09053f
anirudh-bukka <anirudh.bukka@gmail.com>
2023-07-16 12:16:38 +0530

    Added binary search trees - insertion and traversals

--- Trees/src/binarysearchtrees/BuildBST.java
+++ Trees/src/binarysearchtrees/BuildBST.java
@@ -1,2 +1,99 @@
-package binarysearchtrees;public class BuildBST {
+package binarysearchtrees;
+
+import java.util.*;
+
+public class BuildBST {
+
+    static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int value) {
+        if(root == null) {
+            root = new Node(value);
+            return root;
+        }
+
+        if(root.value > value) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    public static void preorderTraversal(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        System.out.print(root.value + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+
+    public static void inorderTraversal(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        inorderTraversal(root.left);
+        System.out.print(root.value + " ");
+        inorderTraversal(root.right);
+    }
+
+    public static void postorderTraversal(Node root) {
+        if(root == null) {
+            return;
+        }
+
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        System.out.print(root.value + " ");
+    }
+
+//    public static void display(Node root) {
+//
+//    }
+
+    public static boolean isPresent(Node root, int key) {
+        if(root == null) {
+            return false;
+        }
+
+        if(root.value == key)
+            return true;
+        else if(root.value > key)
+            return isPresent(root.left, key);
+        else
+            return isPresent(root.right, key);
+    }
+
+    public static void main(String[] args) {
+        int[] ar = {5, 1, 3, 4, 2, 7};
+        Node root = null;
+
+        for(int i = 0; i < ar.length; i++) {
+            root = insert(root, ar[i]);
+        }
+
+        System.out.print("Preorder Traversal: ");
+        preorderTraversal(root);
+        System.out.println();
+        System.out.print("Inorder Traversal: ");
+        inorderTraversal(root);
+        System.out.println();
+        System.out.print("Postorder Traversal: ");
+        postorderTraversal(root);
+        System.out.println("---------------");
+        System.out.println(isPresent(root, 2));
+    }
 }
